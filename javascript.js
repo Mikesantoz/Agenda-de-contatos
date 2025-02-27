19f1aca0408a7f86a9c222cda99fbb6b4b88de28
mike <Mikemarques023@gmail.com>
2025-02-27 11:13:02 -0300

    1

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let form = document.getElementById('formulario-principal');
+
+
+
+
+function adicionar(){
+
+
+
+    const nomes = document.getElementeById('nome').valeu;
+    const numero = document.getElementById('numero-de-telefone').Value;
+
+    alert('funcionando ');
+};
+
+form.addEventListener('submit', adicionar());
